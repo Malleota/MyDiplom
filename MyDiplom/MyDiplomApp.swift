17370e819a143740a7453a9c3da8028cbaaf41a2
Daria <zharko.daria@mail.ru>
2025-11-09 13:34:59 +0300

    Initial Commit

---
+++ MyDiplom/MyDiplomApp.swift
@@ -0,0 +1,17 @@
+//
+//  MyDiplomApp.swift
+//  MyDiplom
+//
+//  Created by Daria Zharko on 09.11.2025.
+//
+
+import SwiftUI
+
+@main
+struct MyDiplomApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
